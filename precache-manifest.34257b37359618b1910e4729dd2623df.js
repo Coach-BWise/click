c5726906144382cae87f134c00d79306b4ac1fb8
Coach-BWise <wisebry11@gmail.com>
2020-05-09 04:06:11 -0400

    Updates

---
+++ precache-manifest.34257b37359618b1910e4729dd2623df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "529c25a851a974cb34c8e254c2689483",
+    "url": "/click/index.html"
+  },
+  {
+    "revision": "9bc0958e68774533a9a7",
+    "url": "/click/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "541d68406a5c5fb51a62",
+    "url": "/click/static/css/main.f0a98e62.chunk.css"
+  },
+  {
+    "revision": "9bc0958e68774533a9a7",
+    "url": "/click/static/js/2.89d6160f.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/click/static/js/2.89d6160f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "541d68406a5c5fb51a62",
+    "url": "/click/static/js/main.cbb2338a.chunk.js"
+  },
+  {
+    "revision": "bac6ea960d024a13ebac",
+    "url": "/click/static/js/runtime-main.d9556c1c.js"
+  }
+]);
